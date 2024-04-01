f56646984aea05bd03b42d49097b6c1e6dd2798d
umikaparnika <165653635+umikaparnika@users.noreply.github.com>
2024-04-01 15:50:11 +0530

    Create NewTestOne.java

---
+++ src/test/java/demo/NewTestOne.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTestOne {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
